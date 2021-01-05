4f584e6934fadad209829055e8762ad440cb019b
dudn1933 <75162982+dudn1933@users.noreply.github.com>
2021-01-05 15:21:15 +0900

    Update cs1Mission1-1.js
    논리연산자와 삼항연산자를 이용하여 간결화시켰다.

--- cs1Mission1-1.js
+++ cs1Mission1-1.js
@@ -1,56 +1,27 @@
+function sum(bitA, bitB) {
+    return bitA && bitB ? false : true;
+};
+
+function carry(bitA, bitB) {
+    return bitA && bitB ? true : false;
+};
+
+//------------------------------------- half adder
 function halfadder(bitA, bitB) {
     let answer = [];
-
-    function halfsum(bitA, bitB) {
-        if (bitA !== bitB) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    function halfcarry(bitA, bitB) {
-        if (bitA && bitB === true) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    answer.push(halfcarry(bitA, bitB), halfsum(bitA, bitB));
+    answer.push(carry(bitA, bitB), sum(bitA, bitB));
     return answer;
 }
+
+//------------------------------------- full adder
 
 function fulladder(bitA, bitB, carry) {
     let answer = [];
-
-    function fullsum(bitA, bitB, carry) {
-        if (carry === true) {
-            if (bitA && bitB === true) {
-                return true;
-            } else {
-                return false;
-            }
-        } else if (carry === false) {
-            if (bitA && bitB === true) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-    }
-
-    function fullcarry(bitA, bitB, carry) {
-        if (carry === true) {
-            return true;
-        } else if (carry == false && (bitA && bitB) === true) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    answer.push(fullcarry(bitA, bitB, carry), fullsum(bitA, bitB, carry))
-    return answer;
+    let adder = sum(sum(bitA,bitB),carry);
+    let upper = adder || carry ? true:false;
+    answer.push(upper,adder)
+    return answer;    
 }
 
-console.log(halfadder(true, false));
+console.log(halfadder(true, true));
 console.log(fulladder(true, true, true));
