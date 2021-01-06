c7c0bb631f4db36340a7e2c02ca79d8c1aeaa0af
dudn1933 <75162982+dudn1933@users.noreply.github.com>
2021-01-06 15:08:21 +0900

    cs1Algorithm
    백준 1009 문제
    Node.js 로 풀기

---
+++ cs1Algorithm.js
@@ -0,0 +1,36 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+//---------------------------------Node.js(백준 1009 문제)
+rl
+    .on('line', function (line) {
+        input.push(line.split(' '));
+    })
+    .on('close', function () {
+        const T = Number(input[0]);
+        for (let i = 1; i <= T; i++) {
+            
+            let a = parseInt(input[i][0])
+            let b = parseInt(input[i][1])
+            
+            let result = a;
+
+            //핵심 포인트 엄청나게 큰 제곱근을 작게 만들어서 연산속도를 대폭 줄일 수 있다.
+            b = b % 4 + 4;
+
+            for (let i = 1; i < b; i++) {
+                result = (result * a) % 10;
+            }
+
+            if (result === 0) result = 10;
+
+            console.log(result);
+        }
+        process.exit();
+    });
