8afe75f1c67a07f6cecf6452c9716eba793a5e4c
dudn1933 <75162982+dudn1933@users.noreply.github.com>
2021-01-08 03:20:41 +0900

    Create cs1Algorithm1076.js

---
+++ cs1Algorithm1076.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let color = ['black','brown','red','orange','yellow','green','blue','violet','grey','white'];
+let input = [];
+let calculate = [];
+
+rl
+    .on('line', function (line) {
+        input.push(line)
+    })
+    .on('close', function () {
+        for(let i = 0; i < input.length; i++) {
+            for(let j = 0; j < color.length; j++) {
+                if(input[i] === color[j]) {
+                    calculate.push(j);
+                }
+            }
+        }       
+        let change = 1 << parseInt(calculate[2]);
+        let result = parseInt(`${calculate[0]}${calculate[1]}`)*parseInt(change.toString(2));
+        console.log(parseInt(result));
+        process.exit();
+    });
